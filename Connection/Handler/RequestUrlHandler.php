97164ead5e64ebd7824e1b77c73ddd1cabb15491
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2019-02-04 13:13:26 +0100

    Backporting code from the app search client.

---
+++ Connection/Handler/RequestUrlHandler.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * This file is part of the Swiftype PHP Client package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Swiftype\Connection\Handler;
+
+use GuzzleHttp\Ring\Core;
+
+/**
+ * This handler add automatically all URIs data to the request.
+ *
+ * @package Swiftype\Connection\Handler
+ *
+ * @author  Aurélien FOUCRET <aurelien.foucret@elastic.co>
+ */
+class RequestUrlHandler
+{
+    /**
+     * @var string
+     */
+    private $uriPrefix = null;
+
+    /**
+     * @var callable
+     */
+    private $handler;
+
+    /**
+     * @var string
+     */
+    private $host;
+
+    /**
+     * @var string
+     */
+    private $scheme;
+
+    /**
+     * Constructor.
+     *
+     * @param callable $handler     original handler
+     * @param string   $apiEndpoint API endpoint (eg. http://myserver/).
+     * @param string   $uriPrefix   A prefix to be added to all URIs?
+     */
+    public function __construct(callable $handler, $apiEndpoint, $uriPrefix = null)
+    {
+        $this->handler = $handler;
+
+        $urlComponents = parse_url($apiEndpoint);
+
+        $this->scheme = $urlComponents['scheme'];
+        $this->host = $urlComponents['host'];
+        $this->uriPrefix = $uriPrefix;
+
+        if (isset($urlComponents['port'])) {
+            $this->host = sprintf('%s:%s', $this->host, $urlComponents['port']);
+        }
+    }
+
+    /**
+     * Add host, scheme and uri prefix to the request before calling the original handler.
+     *
+     * @param array $request original request
+     *
+     * @return array
+     */
+    public function __invoke($request)
+    {
+        $request = Core::setHeader($request, 'host', [$this->host]);
+        $request['scheme'] = $this->scheme;
+
+        if ($this->uriPrefix) {
+            $request['uri'] = $this->addURIPrefix($request['uri']);
+        }
+
+        return ($this->handler)($request);
+    }
+
+    /**
+     * Add prefix for the URI.
+     *
+     * @param string $uri
+     *
+     * @return string
+     */
+    private function addURIPrefix($uri)
+    {
+        return sprintf('%s%s', '/' == substr($uri, 0, 1) ? rtrim($this->uriPrefix, '/') : $this->uriPrefix, $uri);
+    }
+}
---
+++ Connection/Handler/ResponseSerializationHandler.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of the Swiftype Common PHP Client package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Swiftype\Connection\Handler;
+
+use GuzzleHttp\Ring\Core;
+use Swiftype\Serializer\SerializerInterface;
+
+/**
+ * Automatatic serialization of the request params and body.
+ *
+ * @package Swiftype\Connection\Handler
+ *
+ * @author  Aurélien FOUCRET <aurelien.foucret@elastic.co>
+ */
+class ResponseSerializationHandler
+{
+    /**
+     * @var callable
+     */
+    private $handler;
+
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    /**
+     * Constructor.
+     *
+     * @param callable            $handler    original handler
+     * @param SerializerInterface $serializer serialize
+     */
+    public function __construct(callable $handler, SerializerInterface $serializer)
+    {
+        $this->handler = $handler;
+        $this->serializer = $serializer;
+    }
+
+    public function __invoke($request)
+    {
+        $response = Core::proxy(($this->handler)($request), function ($response) use ($request) {
+            if (true === isset($response['body'])) {
+                $response['body'] = stream_get_contents($response['body']);
+                $headers = $response['transfer_stats'] ?? [];
+                $response['body'] = $this->serializer->deserialize($response['body'], $headers);
+            }
+
+            return $response;
+        });
+
+        return $response;
+    }
+}
