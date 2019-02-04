9fd6e5e117eb81188e30a9733e320550e209b3e9
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2019-02-04 12:57:34 +0100

    Use code generator from swiftype-common-php.

--- resources/dev/swiftype-codegen.sh
+++ resources/dev/swiftype-codegen.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 set -e
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ROOT_DIR=`cd $SCRIPT_DIR/../..; pwd`
-GENERATOR_IMAGE=swiftype/swiftype-php-openapi-generator
+argv0=$(echo "$0" | sed -e 's,\\,/,g')
+generatordir=$(dirname "$(readlink "$0" || echo "$argv0")")
 
-FIXER_IMAGE=herloct/php-cs-fixer
+use_winpty=0
 
-cd ${SCRIPT_DIR} && docker build --target runner -t ${GENERATOR_IMAGE} swiftype-php-codegen
+case "$(uname -s)" in
+  *CYGWIN*) generatordir=$(cygpath -w "$basedir");;
+  MSYS*|MINGW*) use_winpty=1;;
+  Linux) generatordir=$(dirname "$(readlink -f "$0" || echo "$argv0")");;
+esac
+echo $generatordir
 
-docker run --rm -v ${ROOT_DIR}:/local ${GENERATOR_IMAGE} generate -g swiftype-php \
-                                                                  -i /local/resources/api/api-spec.yml \
-                                                                  -o /local/ \
-                                                                  -c /local/resources/api/config.json
+generatordir=$(cd $(dirname $argv0) > /dev/null && cd $generatordir > /dev/null && pwd)
+generaroimage=swiftype/swiftype-php-openapi-generator
+rootdir=`cd $(dirname $argv0)/../..; pwd`
+fixerimage="herloct/php-cs-fixer"
+echo $rootdir
 
+cd ${generatordir} && docker build --target runner -t ${generaroimage} swiftype-php-codegen
 
-docker run --rm -v ${ROOT_DIR}:/project ${FIXER_IMAGE} fix --config=.php_cs.dist
+docker run --rm -v ${rootdir}:/local ${generaroimage} generate -g swiftype-php \
+                                                               -i /local/resources/api/api-spec.yml \
+                                                               -o /local/ \
+                                                               -c /local/resources/api/config.json
+
+docker run --rm -v ${rootdir}:/project ${fixerimage} fix --config=.php_cs.dist
